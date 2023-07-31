f66e0abd5b8280263915070df8d32fd6d7e5cb85
RadovanO <radovan.olujic@fourity.com>
2023-07-31 12:06:43 +0200

    added our sign in component as well as firebase utils

--- src/utils/firebase/farebase.utils.js
+++ src/utils/firebase/farebase.utils.js
@@ -34,4 +34,22 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   console.log(userDocRef);
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creatung the user', error.message);
+    }
+  }
+
+  return userDocRef;
 };
