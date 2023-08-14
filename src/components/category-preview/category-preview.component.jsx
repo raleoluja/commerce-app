4ad6cdf016a2e30578e656856fdf68c5320ec116
RadovanO <radovan.olujic@fourity.com>
2023-08-14 16:33:49 +0200

    added nested routes in shop

---
+++ src/components/category-preview/category-preview.component.jsx
@@ -0,0 +1,22 @@
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './category-preview.styles.scss';
+
+const CategoriPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title?.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          ?.filter((_, id) => id < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoriPreview;
--- src/utils/firebase/farebase.utils.js
+++ src/utils/firebase/farebase.utils.js
@@ -9,7 +9,16 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCtOlgjjGOJUMd7gwZI3q1Cux9ynqDS4vs',
@@ -35,6 +44,36 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  field
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+
+  return categoryMap;
+};
+
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
@@ -42,10 +81,7 @@ export const createUserDocumentFromAuth = async (
   if (!userAuth) return;
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  console.log(userDocRef);
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  console.log(userSnapshot.exists());
 
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
