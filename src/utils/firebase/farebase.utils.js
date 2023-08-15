1ad5708a9e4406d51b2cd99957e623f58b57bfaa
RadovanO <radovan.olujic@fourity.com>
2023-08-15 15:27:29 +0200

    some fix

--- src/utils/firebase/farebase.utils.js
+++ src/utils/firebase/farebase.utils.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
@@ -20,14 +20,14 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCtOlgjjGOJUMd7gwZI3q1Cux9ynqDS4vs',
-  authDomain: 'kikicin-shop-db.firebaseapp.com',
-  projectId: 'kikicin-shop-db',
-  storageBucket: 'kikicin-shop-db.appspot.com',
-  messagingSenderId: '628799583587',
-  appId: '1:628799583587:web:ca0f45a27f7def1b308f56',
-};
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyCtOlgjjGOJUMd7gwZI3q1Cux9ynqDS4vs',
+//   authDomain: 'kikicin-shop-db.firebaseapp.com',
+//   projectId: 'kikicin-shop-db',
+//   storageBucket: 'kikicin-shop-db.appspot.com',
+//   messagingSenderId: '628799583587',
+//   appId: '1:628799583587:web:ca0f45a27f7def1b308f56',
+// };
 
 // const firebaseApp = initializeApp(firebaseConfig);
 
