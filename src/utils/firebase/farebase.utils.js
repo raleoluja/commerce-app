a685dadc631da42c123ad2691c6a8584d25f8b62
RadovanO <radovan.olujic@fourity.com>
2023-08-15 15:29:27 +0200

    fix base problem

--- src/utils/firebase/farebase.utils.js
+++ src/utils/firebase/farebase.utils.js
@@ -1,4 +1,4 @@
-// import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
   signInWithRedirect,
@@ -20,16 +20,16 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyCtOlgjjGOJUMd7gwZI3q1Cux9ynqDS4vs',
-//   authDomain: 'kikicin-shop-db.firebaseapp.com',
-//   projectId: 'kikicin-shop-db',
-//   storageBucket: 'kikicin-shop-db.appspot.com',
-//   messagingSenderId: '628799583587',
-//   appId: '1:628799583587:web:ca0f45a27f7def1b308f56',
-// };
+const firebaseConfig = {
+  apiKey: 'AIzaSyCtOlgjjGOJUMd7gwZI3q1Cux9ynqDS4vs',
+  authDomain: 'kikicin-shop-db.firebaseapp.com',
+  projectId: 'kikicin-shop-db',
+  storageBucket: 'kikicin-shop-db.appspot.com',
+  messagingSenderId: '628799583587',
+  appId: '1:628799583587:web:ca0f45a27f7def1b308f56',
+};
 
-// const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
