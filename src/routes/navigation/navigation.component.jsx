145f4910e081eabf02c413c99b4763a9bb2e6010
RadovanO <radovan.olujic@fourity.com>
2023-08-18 15:59:15 +0200

    added redux store

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,14 @@
 import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 import { ReactComponent as Logo } from './../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { selectCurrentUser } from './../../store/user/user.selector';
+
 import { signOutUser } from '../../utils/firebase/farebase.utils';
 
 import {
@@ -16,7 +19,9 @@ import {
 } from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
+  // const { currentUser } = useContext(UserContext);
+
   const { isCartOpen } = useContext(CartContext);
 
   return (
